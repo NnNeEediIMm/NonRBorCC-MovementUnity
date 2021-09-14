1a6902ad421842a500b8a8c9421881c1ba6f9c9e
NnNeEediIMm <83602085+NnNeEediIMm@users.noreply.github.com>
2021-09-14 20:35:56 +0200

    NoRBorCC Movement Unity
    No rigidbody or no character controller movement by NnNeEediIMm (13.)

---
+++ Movement.cs
@@ -0,0 +1,199 @@
+using System.Collections;
+using System;
+using UnityEngine;
+
+/*Non rigidbody and non-
+ character controller
+movement by NnNeEediIMm*/
+
+public class Movement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float speed = 10f;
+
+    float x, z;
+
+    //gravity
+    [Header("Gravity")]
+    public LayerMask ground;
+    public Transform groundCheck;
+    public float gravityScale = 0.1f;
+
+    private float defaultGravityScale;
+    private float distToGround = 0.01f;
+    private bool isGrounded = false;
+
+    //jumping
+    bool jumping;
+    bool canJump = true;
+    [Header("Jumping")]
+    public float jumpForce = 0.1f;
+
+    //crouching
+    [Header("Crouching")]
+    public bool crouching = true;
+    public float reducedSize = 0.5f;
+
+    private float speedWhileCrouching;
+    private float defaultSize;
+    bool crouchingInput;
+    bool crouchingInputUp;
+
+    //sprinting
+    [Header("Sprint")]
+    public bool sprinting = true;
+    public float sprintSpeed = 15f;
+
+    private float defaultSpeed;
+    bool sprint;
+    bool sprintUp;
+
+
+    private void Start()
+    {
+        //gravity
+        defaultGravityScale = gravityScale;
+
+        //Movement
+        speedWhileCrouching = speed / 2;
+        defaultSpeed = speed;
+
+        //crouching
+        defaultSize = transform.localScale.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        myInput();
+
+        //other mechanics
+        Crouching();
+        sprinnting();
+    }
+
+    private void FixedUpdate()
+    {
+        //gravity
+        gravity();
+
+        //jumping
+        jump();
+
+        /*Main movement*/
+        movement();
+    }
+
+    //Gravity
+    public void gravity()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.transform.position, distToGround, ground);
+        if (!isGrounded)
+        {
+            gravityScale = defaultGravityScale;
+            transform.Translate(0f, -gravityScale, 0f);
+        }
+        else
+        {
+            gravityScale = 0f;
+        }
+    }
+    //end of the gravity
+
+    public void myInput()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        jumping = Input.GetKeyDown(KeyCode.Space);
+
+        crouchingInput = Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W);
+        crouchingInputUp = Input.GetKeyUp(KeyCode.LeftShift);
+
+        sprint = Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.W);
+        sprintUp = Input.GetKeyUp(KeyCode.LeftControl);
+    }
+    //jumping
+    public void jump()
+    {
+        if (isGrounded)
+        {
+            if (jumping && canJump)
+            {
+                StartCoroutine(jumpNow());
+                canJump = false;
+            }
+        }
+    }
+    IEnumerator jumpNow()
+    {
+        for (int jump = 0; jump < 8; jump++)
+        {
+            yield return new WaitForSeconds(0.000000000000000000000000001f);
+            transform.Translate(0, 0.1f + jumpForce, 0);
+        }
+        yield return new WaitForSeconds(0.19f);
+        canJump = true;
+    }
+
+    /// <summary>
+    /// Other mechanics of the movement!!
+    /// Crouching
+    /// </summary>
+    public void Crouching()
+    {
+        if (isGrounded && crouching)
+        {
+            if (crouchingInput)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, reducedSize, transform.localScale.z);
+            }
+            if (!crouchingInput)
+            {
+                StopCrouching();
+                normalPositionAfterCrouching();
+            }
+        }
+    }
+    public void StopCrouching()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, defaultSize, transform.localScale.z);
+    }
+    public void normalPositionAfterCrouching()
+    {
+        if (crouchingInputUp || crouchingInput && transform.localScale.y > 0.51)
+        {
+            transform.Translate(Vector3.up * 2);
+        }
+    }
+    //sprinting 
+    public void sprinnting()
+    {
+        if (isGrounded && sprinting)
+        {
+            if (sprint)
+            {
+                speed = sprintSpeed;
+            }
+            if (sprintUp)
+            {
+                normalSpeed();
+            }
+        }
+    }
+    public void normalSpeed()
+    {
+        speed = defaultSpeed;
+    }
+
+    /// <summary>
+    /// Movement 
+    /// for all script!
+    /// </summary>
+    public void movement() 
+    {
+        transform.Translate(Vector3.right * x * Time.fixedDeltaTime * speed);
+        transform.Translate(Vector3.forward * z * Time.fixedDeltaTime * speed);
+    }
+}
